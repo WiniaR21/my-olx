432326a829519bdad718be058d22d21132093786
unknown <daniel.winiarczyk18@gmail.com>
2024-04-23 14:44:33 +0200

    (feat:CarAnnouncement) Base entity

---
+++ server/services/cars/src/main/java/com/myolx/cars/entity/CarAnnouncement.java
@@ -0,0 +1,18 @@
+package com.myolx.cars.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class CarAnnouncement {
+    @Id
+    private Long carAnnouncementId;
+
+    @Column(name = "belongs_to")
+    private String belongsTo;
+}
